5507c1a8ac1829dee954e2130a70d3def9635412
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-24 22:51:14 +0100

    feat: add prompt and function result

---
+++ palindromi/js/main.js
@@ -0,0 +1,19 @@
+// test invoco funzione
+// const wordResult_1= palindrome('Nabil')
+// console.log(wordResult_1)
+
+// const wordResult_2 = palindrome('Anna')
+// console.log(wordResult_2)
+
+// const wordResult_3 = palindrome('Marco')
+// console.log(wordResult_3)
+
+
+// prompt
+const userWord = prompt('Dammi una parola');
+
+// invoco funzione
+const wordResult= palindrome(userWord)
+
+// stampo parola
+console.log(wordResult)
