aca3b758ab904fd782280b578b275c7353ff7abc
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-24 23:50:21 +0100

    feat: add prompts

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,7 @@
+// chiedo numero
+const userNum = prompt('Dammi un numero');
+console.log(userNum)
+
+// chiedo pari o dispari?
+const userChoice = prompt('Scegli pari o dispari?');
+console.log(userChoice)
